bf3e0ec2a9b2db1b1711e079a956851d8061c715
Carl Antiado <carl.antiado01@gmail.com>
2023-04-15 16:40:47 -0700

    Added image uploading

--- frontend/src/components/MainContainer.vue
+++ frontend/src/components/MainContainer.vue
@@ -1,18 +1,26 @@
 <script setup>
-import GetStarted from './GetStarted.vue';
-import ConfirmImage from './ConfirmImage.vue';
-import UploadImage from './UploadImage.vue';
-import LoadingView from './LoadingView.vue';
-import ClassifiedResult from './ClassifiedResult.vue';
+import { ref } from "vue";
+import GetStarted from "./GetStarted.vue";
+import ConfirmImage from "./ConfirmImage.vue";
+import UploadImage from "./UploadImage.vue";
+import LoadingView from "./LoadingView.vue";
+import ClassifiedResult from "./ClassifiedResult.vue";
+
+const index = ref(0);
+
+function uploadImage(file) {
+  console.log(file)
+  index.value = 1;
+}
 </script>
 
 <template>
-    <div class="h-screen w-screen grid grid-cols-1 fliex items-stretch bg-mellow-yellow p-5 pt-10">
-        <!-- Component views go here -->
-        <!-- <GetStarted /> -->
-        <!-- <UploadImage /> -->
-        <!-- <ConfirmImage /> -->
-        <!-- <LoadingView /> -->
-        <ClassifiedResult />
-    </div>
+  <div class="h-screen w-full bg-mellow-yellow p-10">
+    <!-- Component views go here -->
+    <!-- <GetStarted v-if="index == 0" /> -->
+    <UploadImage v-if="index == 0" @submit="uploadImage" />
+    <!-- <ConfirmImage v-if="index == 2" /> -->
+    <LoadingView v-if="index == 1" />
+    <ClassifiedResult v-if="index == 2" />
+  </div>
 </template>
