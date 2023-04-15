6376de2930460b43dfaf43b2bc24b4e65a943709
Diane <dianedu@nevada.unr.edu>
2023-04-14 21:37:02 -0700

    got tailwind installed and working for styling. created navbar component

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="flex flex-row justify-between p-5 bg-unripe-green">
+        <img src=".../assets/logo.svg" alt="Banyaynay logo">
+        <h1>Banyaynay</h1>
+        <div></div>
+    </nav>
+</template>
+
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'unripe-green': '#d9f99d',
+      },
+    },
+  },
+  plugins: [],
+}
+
