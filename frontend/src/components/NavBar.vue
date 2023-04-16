e4de8a5ba57245598ea24e6ad8d4acdd1bec46fa
Diane <dianedu@nevada.unr.edu>
2023-04-15 21:02:24 -0700

    minor adjustments

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="flex justify-between p-3 bg-gray-900 rounded-sm ">
-            <h1 class="text-3xl text-white">Banyaynay</h1>
-            <img 
+        <h1 class="text-3xl text-white">Banyaynay</h1>
+        <img 
                 src="../assets/banyaynay-logo.png" 
                 alt="Banyaynay"
                 class="h-10"
-            />
+            /> 
     </nav>
 </template>
 
