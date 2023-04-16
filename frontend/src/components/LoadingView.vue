b6ddbd49c8717993fa2b10d84645be22a54218b1
Diane <dianedu@nevada.unr.edu>
2023-04-15 19:14:20 -0700

    adding loading gif

--- frontend/src/components/LoadingView.vue
+++ frontend/src/components/LoadingView.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="p-5 flex flex-col">
-        <h2 class="text-2xl">Loading...</h2>   
+        <h2 class="text-4xl">Loading...</h2>
+        <img src="https://media.giphy.com/media/cw8Nr4u28tVKw/giphy.gif" 
+            class="h-1/2 self-center mt-10"/>
     </div>
 </template>
