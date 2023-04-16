8749aeeaa5bedf6de758e870b4694fda98765819
Diane <dianedu@nevada.unr.edu>
2023-04-15 20:18:00 -0700

    Included logo in navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,11 @@
 <template>
-    <nav class="flex justify-center p-3 bg-gray-900 rounded-sm w-full">
+    <nav class="flex justify-between p-3 bg-gray-900 rounded-sm ">
             <h1 class="text-3xl text-white">Banyaynay</h1>
-            <div></div>
+            <img 
+                src="../assets/banyaynay-logo.png" 
+                alt="Banyaynay"
+                class="h-10"
+            />
     </nav>
 </template>
 
